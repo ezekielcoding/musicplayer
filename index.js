4be55d490c69eff35c030e9a338c8d6f0d6c0c17
Zeke <130468038+ezekielcoding@users.noreply.github.com>
2024-04-26 22:32:06 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,174 @@
+const image = document.getElementById('cover'),
+    title = document.getElementById('music-title'),
+    artist = document.getElementById('music-artist'),
+    currentTimeEl = document.getElementById('current-time'),
+    durationEl = document.getElementById('duration'),
+    progress = document.getElementById('progress'),
+    playerProgress = document.getElementById('player-progress'),
+    prevBtn = document.getElementById('prev'),
+    nextBtn = document.getElementById('next'),
+    playBtn = document.getElementById('play'),
+    background = document.getElementById('bg-img');
+
+const music = new Audio();
+
+const songs = [
+    {
+        path: 'assets/1.mp3',
+        displayName: 'XO Tour Llif3',
+        cover: 'assets/1.jpg',
+        artist: 'Lil Uzi Vert',
+    },
+    {
+        path: 'assets/2.mp3',
+        displayName: 'UR THE MOON',
+        cover: 'assets/2.jpg',
+        artist: 'Playboi Carti',
+    },
+    {
+        path: 'assets/3.mp3',
+        displayName: 'Come Through',
+        cover: 'assets/3.jpg',
+        artist: 'The Weeknd',
+    },
+    {
+        path: 'assets/4.mp3',
+        displayName: 'FIFTY',
+        cover: 'assets/4.jpg',
+        artist: 'YEAT - [prod. R3DD x SHARKBOY]',
+    },
+    {
+        path: 'assets/5.mp3',
+        displayName: 'She Said Shes From The Islands',
+        cover: 'assets/5.jpg',
+        artist: 'Tomo x Frozy',
+    },
+    {
+        path: 'assets/6.mp3',
+        displayName: 'Lil Krazy',
+        cover: 'assets/6.jpg',
+        artist: 'YEAT - [prod. TRGC, Darkoivx, pooka]',
+    },
+    {
+        path: 'assets/7.mp3',
+        displayName: 'Faster Than Lightning',
+        cover: 'assets/7.jpg',
+        artist: 'DJ Steve Porter',
+    },
+    {
+        path: 'assets/8.mp3',
+        displayName: 'Cash Shit',
+        cover: 'assets/8.jpg',
+        artist: 'Playboi Carti',
+    },
+    {
+        path: 'assets/9.mp3',
+        displayName: '24 Songs',
+        cover: 'assets/9.jpg',
+        artist: 'Playboi Carti',
+    },
+    {
+        path: 'assets/10.mp3',
+        displayName: 'Southside',
+        cover: 'assets/10.jpg',
+        artist: 'Playboi Carti',
+    },
+    {
+        path: 'assets/11.mp3',
+        displayName: 'X',
+        cover: 'assets/11.jpg',
+        artist: 'Fedmate',
+    },
+    {
+        path: 'assets/12.mp3',
+        displayName: 'I LUV IT [CARTI ONLY]',
+        cover: 'assets/12.jpg',
+        artist: 'Playboi Carti',
+    },
+    {
+        path: 'assets/13.mp3',
+        displayName: 'SDP Interlude',
+        cover: 'assets/13.jpg',
+        artist: 'Travis Scott',
+    },
+    {
+        path: 'assets/14.mp3',
+        displayName: 'Havana [YOUNG THUG ONLY]',
+        cover: 'assets/14.jpg',
+        artist: 'Young Thug',
+    },
+    {
+        path: 'assets/15.mp3',
+        displayName: 'Pop Out',
+        cover: 'assets/15.jpg',
+        artist: 'Playboi Carti'
+    }
+];
+
+let musicIndex = 0;
+let isPlaying = false;
+
+function togglePlay() {
+    if (isPlaying) {
+        pauseMusic();
+    } else {
+        playMusic();
+    }
+}
+
+function playMusic() {
+    isPlaying = true;
+    // Change play button icon
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    // Set button hover title
+    playBtn.setAttribute('title', 'Pause');
+    music.play();
+}
+
+function pauseMusic() {
+    isPlaying = false;
+    // Change pause button icon
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    // Set button hover title
+    playBtn.setAttribute('title', 'Play');
+    music.pause();
+}
+
+function loadMusic(song) {
+    music.src = song.path;
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    image.src = song.cover;
+    background.src = song.cover;
+}
+
+function changeMusic(direction) {
+    musicIndex = (musicIndex + direction + songs.length) % songs.length;
+    loadMusic(songs[musicIndex]);
+    playMusic();
+}
+
+function updateProgressBar() {
+    const { duration, currentTime } = music;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+
+    const formatTime = (time) => String(Math.floor(time)).padStart(2, '0');
+    durationEl.textContent = `${formatTime(duration / 60)}:${formatTime(duration % 60)}`;
+    currentTimeEl.textContent = `${formatTime(currentTime / 60)}:${formatTime(currentTime % 60)}`;
+}
+
+function setProgressBar(e) {
+    const width = playerProgress.clientWidth;
+    const clickX = e.offsetX;
+    music.currentTime = (clickX / width) * music.duration;
+}
+
+playBtn.addEventListener('click', togglePlay);
+prevBtn.addEventListener('click', () => changeMusic(-1));
+nextBtn.addEventListener('click', () => changeMusic(1));
+music.addEventListener('ended', () => changeMusic(1));
+music.addEventListener('timeupdate', updateProgressBar);
+playerProgress.addEventListener('click', setProgressBar);
+
+loadMusic(songs[musicIndex]);
